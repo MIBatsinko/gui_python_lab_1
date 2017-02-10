a48cc5a59ebaecbb6c27b217839759136906b40b
MIBatsinko <pochta.haha@gmail.com>
2017-02-10 19:46:00 +0200

    Add files via upload

---
+++ singleton.py
@@ -0,0 +1,17 @@
+class Singleton():
+  def __new__(this, *args, **kwargs):
+      if not hasattr(this, '_instance'):
+            orig = super(Singleton, this)
+            this._instance = orig.__new__(this, *args, **kwargs)
+      return this._instance
+
+
+if __name__ == '__main__':
+    class ClassS(Singleton):
+        pass
+    test1_1, test1_2 = ClassS(), ClassS()
+    test2_1, test2_2 = Singleton(), Singleton()
+
+    print('test1_1:  %s\ntest1_2: %s' % (test1_1, test1_2))
+    print()
+    print('test2_1:  %s\ntest2_2: %s' % (test2_1, test2_2))
