5817b2daf1c12fa06336fa011d4e41b9ca365f72
Maksim Batsinko <pochta.haha@gmail.com>
2017-02-24 09:45:22 +0200

    Update class.py

--- lab 2/class.py
+++ lab 2/class.py
@@ -1,6 +1,27 @@
-class Deck:
-    pass
+import random
 
-class Card:
-    pass
+class Card(object):
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
 
+    def __str__(self):
+        return "%s%s" % (self.rank, self.suit)
+    
+class Deck(object):
+    def __init__(self):
+        ranks = "23456789TJQKA"
+        #Spades, Clubs, Hearts, Diamonds
+        suits = "SCHD"
+        self.cards = [Card(r, s) for r in ranks for s in suits]
+    def get_cards(self):
+        for i in range(52):
+            print(self.cards[i])
+    def shuffle_cards(self):
+        random.shuffle(self.cards)
+
+if __name__ == "__main__":
+    a = Deck()
+    print(a.get_cards(), '\n\nAfter shuffle:')
+    a.shuffle_cards()
+    print(a.get_cards())
